98c98e6564726a8fb09b2282f74019b76a06cd76
Shubhagata Sengupta <shubsengupta@gmail.com>
2014-09-21 04:25:36 -0400

    yolo moxtra somewhat progress

---
+++ sample-videochat/VideoChat-sample-ios/ViewController/ChatRatingViewController/ChatRatingViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatRatingViewController.h
+//  VideoChat
+//
+//  Created by Shub Sengupta on 2014-09-21.
+//  Copyright (c) 2014 Ruslan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatRatingViewController : UIViewController
+
+@end
---
+++ sample-videochat/VideoChat-sample-ios/ViewController/ChatRatingViewController/ChatRatingViewController.m
@@ -0,0 +1,38 @@
+//
+//  ChatRatingViewController.m
+//  VideoChat
+//
+//  Created by Shub Sengupta on 2014-09-21.
+//  Copyright (c) 2014 Ruslan. All rights reserved.
+//
+
+#import "ChatRatingViewController.h"
+#import "Moxtra.h"
+
+@interface ChatRatingViewController ()
+
+@end
+
+@implementation ChatRatingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
