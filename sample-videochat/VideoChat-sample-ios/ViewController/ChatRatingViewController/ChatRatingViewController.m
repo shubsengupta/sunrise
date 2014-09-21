3217fa0f552e04c3388ddff5b0c60e720337950c
Shubhagata Sengupta <shubsengupta@gmail.com>
2014-09-21 09:12:08 -0400

    almost working

--- sample-videochat/VideoChat-sample-ios/ViewController/ChatRatingViewController/ChatRatingViewController.m
+++ sample-videochat/VideoChat-sample-ios/ViewController/ChatRatingViewController/ChatRatingViewController.m
@@ -25,6 +25,64 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)chatOpenButton:(id)sender {
+    [self startChat:nil];
+    
+    // Fill in the App Client ID and Client Secret Key received from the app registration step from Moxtra
+    NSString *APP_CLIENT_ID = @"ug3O_xKRTs4";
+    NSString *APP_CLIENT_SECRET = @"k7Lexii75Jg";
+    
+    // Set up Moxtra SDK
+    [Moxtra clientWithApplicationClientID:APP_CLIENT_ID applicationClientSecret:APP_CLIENT_SECRET];
+//
+//    // Initialize user using unique user identity
+//    MXUserIdentity *useridentity = [[MXUserIdentity alloc] init];
+//    useridentity.userIdentityType = kUserIdentityTypeIdentityUniqueID;
+//    useridentity.userIdentity = @"user unique identity";
+//    
+//    [[Moxtra sharedClient]
+//     initializeUserAccount: useridentity
+//     firstName: @"John"
+//     lastName: @"Doe"
+//     avatar: nil
+//     devicePushNotificationToken: nil
+//     success: ^{
+//         NSLog(@"Setup user account successfully");
+//     } failure: ^(NSError *error) {
+//         NSLog(@"Setup user account failed, %@" , [NSString stringWithFormat:@"error code [%d] description: [%@] info [%@]", [error code], [error localizedDescription], [[error userInfo] description]]);
+//     }];
+//    
+//}
+
+MXUserIdentity *useridentity = [[MXUserIdentity alloc] init];
+useridentity.userIdentityType = kUserIdentityTypeEmail;
+
+[[Moxtra sharedClient]
+ initializeUserAccount: useridentity
+ firstName: nil
+ lastName: nil
+ avatar: nil
+ devicePushNotificationToken: nil
+ success: ^{
+     NSLog(@"Setup user account successfully");
+ } failure: ^(NSError *error) {
+     NSLog(@"Setup user account failed, %@", [NSString stringWithFormat:@"error code [%d] description: [%@] info [%@]", [error code], [error localizedDescription], [[error userInfo] description]]);
+ }];
+
+- (void)startChat:(id)sender
+{
+    [[Moxtra sharedClient] createChat:CGRectMake(290, 538, 50, 50) success:^(NSString *conversationID) {
+        NSLog(@"start conversation success, id = %@", conversationID);
+        //self.conversationID1 = conversationID;
+    } failure:^(NSError *error) {
+        NSLog(@"start conversation failed");
+    }];
+    
+    [[Moxtra sharedClient] setDelegate:self];
+    return;
+}
+
+
 /*
 #pragma mark - Navigation
 
@@ -34,5 +92,6 @@
     // Pass the selected object to the new view controller.
 }
 */
-
+}
 @end
+
